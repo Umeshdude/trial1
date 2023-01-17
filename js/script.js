3707dfa7e3d86be2f897f74472c870790ec926ec
nitesh1803 <40404816+nitesh1803@users.noreply.github.com>
2023-01-17 23:46:21 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@
 
 })(jQuery);
 
-$(document).on('load', function(){
+$(document).on('click', function(){
     document.getElementById("my_audio").play();
     console.log('Shaadi me zaroor aana');
 });
